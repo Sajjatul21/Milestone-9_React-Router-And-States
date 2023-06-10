e8df8afc592ebefd89c01384156e6a3b9b54e195
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-10 08:14:10 +0100

    create home component  and add Home compoonent in the element

--- 52 Simple React Router Examples/explore-routes/src/App.js
+++ 52 Simple React Router Examples/explore-routes/src/App.js
@@ -1,17 +1,17 @@
 
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home';
 
 function App() {
   const router = createBrowserRouter([
     { path: '/', element: <div>This is Default page</div> },
-    { path: '/home', element: <div>Home page</div> },
+    { path: '/home', element: <Home></Home> },
     { path: '/about', element: <div>This is About page</div> }
   ]);
   return (
     <div className="App">
       <RouterProvider router={router}></RouterProvider>
-      {/* 52-2 Create Simple Route with Links and Components */}
     </div>
   );
 }
