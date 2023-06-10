02db33109d1551f94cbfba96ab25c539595d322c
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-10 11:55:24 +0100

    আমরা এবার defaul page এ Main component টা বসাই দিলাম যেখোনে
    header componentেআছে আমরা এবার Main এর child হিসাবে home and about বসাই দিলাম

--- 52 Simple React Router Examples/explore-routes/src/App.js
+++ 52 Simple React Router Examples/explore-routes/src/App.js
@@ -4,18 +4,23 @@ import './App.css';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Products from './components/Products/Products';
+import Main from './layout/Main';
 
 function App() {
   const router = createBrowserRouter([
-    { path: '/', element: <div>This is Default page</div> },
-    { path: '/home', element: <Home></Home> },
-    { path: '/about', element: <About></About> },
+    {
+      path: '/', element: <Main></Main>, children: [
+        { path: '/', element: <Home></Home> },
+        { path: '/home', element: <Home></Home> },
+        { path: '/about', element: <About></About> },
+      ]
+    },
+
     { path: 'products', element: <Products></Products> }
   ]);
   return (
     <div className="App">
       <RouterProvider router={router}></RouterProvider>
-      {/* 52-3 (interesting) Nested route and outlet for shared content */}
     </div>
   );
 }
