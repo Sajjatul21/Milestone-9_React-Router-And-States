5ba1c41c4ed1fefaad96c8dd34e21e6dc5b9eac7
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-10 13:13:52 +0100

    এবার আমরা user name এর উপর মাউচ রাখলে আমরা /friend//friend/1 etc দেখতে পাই

--- 52 Simple React Router Examples/explore-routes/src/components/Friend/Friend.js
+++ 52 Simple React Router Examples/explore-routes/src/components/Friend/Friend.js
@@ -3,12 +3,12 @@ import './Friend.css';
 import { Link } from 'react-router-dom';
 
 const Friend = ({ friend }) => {
-    const { name, email, username } = friend;
+    const { id, name, email, username } = friend;
     return (
         <div className='friend'>
             <h3>Name: {name}</h3>
             <p>Email: {email}</p>
-            <p><small>User Name: <Link to='/friend'>{username}</Link></small></p>
+            <p><small>User Name: <Link to={`/friend/${id}`}>{username}</Link></small></p>
         </div>
     );
 };
