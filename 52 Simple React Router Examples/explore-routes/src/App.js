ff8e850a88375f44055fd8bd8beae2a70ac7fc80
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-10 07:45:27 +0100

    create few route

--- 52 Simple React Router Examples/explore-routes/src/App.js
+++ 52 Simple React Router Examples/explore-routes/src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 
 function App() {
   const router = createBrowserRouter([
-
+    { path: '/', element: <div>This is Default page</div> },
+    { path: '/home', element: <div>Home page</div> },
+    { path: '/about', element: <div>This is About page</div> }
   ]);
   return (
     <div className="App">
--- 52 Simple React Router Examples/explore-routes/src/__steps.js
+++ 52 Simple React Router Examples/explore-routes/src/__steps.js
@@ -2,4 +2,5 @@
     1. install react router: npm i react-router-dom
     2. crate a router using createBrowserRouter
     3. add RouterProbider and pass router
+    4. create few route
 */
