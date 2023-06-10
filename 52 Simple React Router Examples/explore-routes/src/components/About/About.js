13639ea6165245f2f14544edb6cc37bed5bc9664
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-10 08:23:19 +0100

    আমরা এখন Home and About click করলে page টা হালকা একটা reload
    নিচ্ছে single page application এই page reload নিবে না

--- 52 Simple React Router Examples/explore-routes/src/components/About/About.js
+++ 52 Simple React Router Examples/explore-routes/src/components/About/About.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../Header/Header';
 
 const About = () => {
     return (
         <div>
+            <Header></Header>
             <h3>This is true Real About me page</h3>
         </div>
     );
--- 52 Simple React Router Examples/explore-routes/src/components/Home/Home.js
+++ 52 Simple React Router Examples/explore-routes/src/components/Home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../Header/Header';
 
 const Home = () => {
     return (
         <div>
+            <Header></Header>
             <h3>This is the real component</h3>
         </div>
     );
