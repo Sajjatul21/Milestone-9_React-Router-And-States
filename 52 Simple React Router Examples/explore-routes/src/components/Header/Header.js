8313577932ab8934126685b7123b83cd90081d73
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-10 08:27:02 +0100

    we are using LInk for single page application

--- 52 Simple React Router Examples/explore-routes/src/components/Header/Header.js
+++ 52 Simple React Router Examples/explore-routes/src/components/Header/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
             <a href="/home">Home</a>
             <a href="/about">About</a>
+            <br />
+            <Link to='/home'>Home</Link>
+            <Link to='/about'>About</Link>
         </div>
     );
 };
