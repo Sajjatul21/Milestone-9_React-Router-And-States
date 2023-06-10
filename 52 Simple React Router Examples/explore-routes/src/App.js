ce66d118668fa571599a0b68200b781a77ef33ff
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-10 05:27:17 +0100

    52-1 Module Introduction, simple react router setup

---
+++ 52 Simple React Router Examples/explore-routes/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      {/* 52-1 Module Introduction, simple react router setup */}
+      
+    </div>
+  );
+}
+
+export default App;
