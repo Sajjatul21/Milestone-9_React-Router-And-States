5b0927bf8d45ee9c0df439c3e272be74b4d883ee
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-10 12:58:05 +0100

    create friend component . using map

--- 52 Simple React Router Examples/explore-routes/src/components/Friends/Friends.js
+++ 52 Simple React Router Examples/explore-routes/src/components/Friends/Friends.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Friend from '../Friend/Friend';
 
 const Friends = () => {
     const friends = useLoaderData();
-    console.log(friends);
+
     return (
         <div>
             <h4>I have so many friends.YAY!!!!!: {friends.length}</h4>
+            {
+                friends.map(friend => <Friend
+                    key={friend.id}
+                    friend={friend}
+                >
+                </Friend>)
+            }
         </div>
     );
 };
