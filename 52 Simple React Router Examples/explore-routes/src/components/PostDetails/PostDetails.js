f71b3be08387584a1ececea2744a283f1107fb18
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-15 17:10:16 +0100

    create another link

--- 52 Simple React Router Examples/explore-routes/src/components/PostDetails/PostDetails.js
+++ 52 Simple React Router Examples/explore-routes/src/components/PostDetails/PostDetails.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 
 const PostDetails = () => {
     const post = useLoaderData();
-    const { id, title, body } = post;
+    // console.log(post);
+    const { id, title, body, userId } = post;
+    const navigate = useNavigate();
+    const handleNavigate = () => {
+        navigate(`/friend/${userId}`);
+    };
     return (
         <div>
             <h1>Details about Post: {id}</h1>
             <p>{title}</p>
             <p><small>{body}</small></p>
+            <button onClick={handleNavigate}>Get the Author</button>
         </div>
     );
 };
