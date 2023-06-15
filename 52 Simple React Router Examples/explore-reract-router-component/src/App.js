de36d0273ba31f950fc9c9446f5e5a83fcfcff28
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-16 00:59:16 +0100

    create route another way

--- 52 Simple React Router Examples/explore-reract-router-component/src/App.js
+++ 52 Simple React Router Examples/explore-reract-router-component/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Products from './components/Products';
 
 function App() {
   return (
     <div className="App">
-     
+      <Routes>
+        <Route path='/' element={<Home></Home>} ></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/products' element={<Products></Products>}></Route>
+      </Routes>
     </div>
   );
 }
