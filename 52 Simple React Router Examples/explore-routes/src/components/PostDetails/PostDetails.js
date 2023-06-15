8d6e7ece892bfbc7fc8de79470265285962c6276
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-15 17:02:40 +0100

    set title and body

--- 52 Simple React Router Examples/explore-routes/src/components/PostDetails/PostDetails.js
+++ 52 Simple React Router Examples/explore-routes/src/components/PostDetails/PostDetails.js
@@ -3,10 +3,12 @@ import { useLoaderData } from 'react-router-dom';
 
 const PostDetails = () => {
     const post = useLoaderData();
-    const { id } = post;
+    const { id, title, body } = post;
     return (
         <div>
             <h1>Details about Post: {id}</h1>
+            <p>{title}</p>
+            <p><small>{body}</small></p>
         </div>
     );
 };
