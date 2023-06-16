0b6ab1f916da4587e4cbb185e859ab32335e9f29
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-16 04:27:54 +0100

    create link

--- 52 Simple React Router Examples/3-routed-countries/src/components/Countries.js
+++ 52 Simple React Router Examples/3-routed-countries/src/components/Countries.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Countries = () => {
     const countries = useLoaderData();
+    console.log(countries);
     return (
         <div>
             <h1>All Countries {countries.length}</h1>
+            {
+                countries.map(country => <li key={country.cca3}>
+                    <Link to={``}>{country.name.common}</Link>
+                </li>)
+            }
         </div>
     );
 };
