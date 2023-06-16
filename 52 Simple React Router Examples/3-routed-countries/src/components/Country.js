3d3c75a01846debe1de5b2e093009d8ef46fa175
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-16 04:43:39 +0100

    show flag and name and region

--- 52 Simple React Router Examples/3-routed-countries/src/components/Country.js
+++ 52 Simple React Router Examples/3-routed-countries/src/components/Country.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Country = () => {
-    const country = useLoaderData();
-    console.log(country);
+    const country = useLoaderData()[0];
+    // console.log(country);
     return (
         <div>
+            <img src={country.flags.png} alt="" />
+            <h3>Name: {country.name.common}</h3>
+            <p>Region: {country.region}</p>
 
         </div>
     );
