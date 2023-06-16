66435e2e563cdbf45865b0d28e0233d945e57b47
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-16 03:53:58 +0100

    crete router on another file

--- 52 Simple React Router Examples/3-routed-countries/src/routes/routes.js
+++ 52 Simple React Router Examples/3-routed-countries/src/routes/routes.js
@@ -1,1 +1,6 @@
-const routes =
+import { createBrowserRouter } from "react-router-dom";
+import Countries from "../components/Countries";
+
+export const router = createBrowserRouter([
+    { path: '/', element: <Countries></Countries> }
+]);
