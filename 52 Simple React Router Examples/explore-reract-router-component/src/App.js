31e06be6b52b0ad30012a7a26127afeb61818f38
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-16 01:10:14 +0100

    create link

--- 52 Simple React Router Examples/explore-reract-router-component/src/App.js
+++ 52 Simple React Router Examples/explore-reract-router-component/src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import Home from './components/Home';
 import About from './components/About';
 import Products from './components/Products';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>} ></Route>
+        <Route path='home' element={<Home></Home>} ></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/products' element={<Products></Products>}></Route>
       </Routes>
