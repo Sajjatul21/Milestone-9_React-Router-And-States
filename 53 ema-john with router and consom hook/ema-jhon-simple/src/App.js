855ea75fbe9b96a1a97cc95373e81e9859574eb6
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-20 02:24:48 +0100

    we are set Outlet and as children set shop component

--- 53 ema-john with router and consom hook/ema-jhon-simple/src/App.js
+++ 53 ema-john with router and consom hook/ema-jhon-simple/src/App.js
@@ -2,12 +2,19 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import Main from "./layouts/Main";
 import About from './component/About/About';
+import Shop from './component/Shop/Shop';
 
 function App() {
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <Main></Main>
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Shop></Shop>
+        }
+      ]
     },
     {
       path: 'about',
--- 53 ema-john with router and consom hook/ema-jhon-simple/src/layouts/Main.js
+++ 53 ema-john with router and consom hook/ema-jhon-simple/src/layouts/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 import Header from '../component/Header/Header';
-import Shop from '../component/Shop/Shop';
+
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
-            <Shop></Shop>
+            <Outlet></Outlet>
+            
 
             {/* এবার আমরা চাচ্চি header টা common থাকবে বা সব সময় থাকবে আর shop টা change হবে।  commot অংশ টা যাবে layout এর আর বাকি অংশটা যেটা route অনুসারে change হবে সেটা Outlet এর যাবে।  */}
 
