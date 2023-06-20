9cfa7190b4b6973358ba92b9b8b9fc953da31522
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-20 01:53:39 +0100

    make layouts and ceate browser router

--- 53 ema-john with router and consom hook/ema-jhon-simple/src/App.js
+++ 53 ema-john with router and consom hook/ema-jhon-simple/src/App.js
@@ -1,13 +1,18 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
-import Header from './component/Header/Header';
-import Shop from './component/Shop/Shop';
-
+import Main from "./layouts/Main";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>
+
+    }
+  ]);
   return (
     <div>
-      <Header></Header>
-      <Shop></Shop>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
