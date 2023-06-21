3e73a3fd5f8355bf931a46e1c93efd6b28176a8e
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-21 04:25:48 +0100

    use flex

--- 53 ema-john with router and consom hook/ema-jhon-simple/src/component/ReviewItem/ReviewItem.js
+++ 53 ema-john with router and consom hook/ema-jhon-simple/src/component/ReviewItem/ReviewItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './ReviewItem.css';
 const ReviewItem = ({ product }) => {
 
-    const { name, price, quentity, img } = product;
+    const { name, price, quantity, img } = product;
     return (
         <div className='review-item'>
             <div>
@@ -11,6 +11,8 @@ const ReviewItem = ({ product }) => {
             <div className="review-details-container">
                 <div className="review-details">
                     <p>{name}</p>
+                    <p><small>Price: {price}</small></p>
+                    <p><small>Quantity: {quantity}</small></p>
                 </div>
                 <div className="delete-container">
                     <button>delete</button>
