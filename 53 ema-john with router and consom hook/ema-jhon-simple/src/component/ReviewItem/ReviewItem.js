15729ddb94235ea82245cf505bcc05398740ce22
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-21 04:20:29 +0100

    add name, img btn

--- 53 ema-john with router and consom hook/ema-jhon-simple/src/component/ReviewItem/ReviewItem.js
+++ 53 ema-john with router and consom hook/ema-jhon-simple/src/component/ReviewItem/ReviewItem.js
@@ -1,11 +1,22 @@
 import React from 'react';
-
+import './ReviewItem.css';
 const ReviewItem = ({ product }) => {
-    console.log(product);
-    const { name, price, quentity } = product;
+
+    const { name, price, quentity, img } = product;
     return (
-        <div>
-            <h2>This is Review Item</h2>
+        <div className='review-item'>
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div className="review-details-container">
+                <div className="review-details">
+                    <p>{name}</p>
+                </div>
+                <div className="delete-container">
+                    <button>delete</button>
+                </div>
+            </div>
+
         </div>
     );
 };
