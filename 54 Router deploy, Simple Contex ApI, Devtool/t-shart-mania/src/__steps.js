2a0f5f98ea016dd25570f57e550f1b1765598837
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-23 21:38:14 +0100

    setps

--- 54 Router deploy, Simple Contex ApI, Devtool/t-shart-mania/src/__steps.js
+++ 54 Router deploy, Simple Contex ApI, Devtool/t-shart-mania/src/__steps.js
@@ -1,8 +1,11 @@
 /* 
+Basic Router Setup ->
     1. npm i react-router-dom
     2. create a router using createBrowserRouter 
     3. add RouterProvider and set router prop
     4. Create a lot of component
     5. create Main layout with some common part and one changing  based on the route
-    6.
+    6. set main layout at root of the  router 
+    7. set Children routers
+    8. set Link on the header component
 */
--- 54 Router deploy, Simple Contex ApI, Devtool/t-shart-mania/src/components/Header/Header.js
+++ 54 Router deploy, Simple Contex ApI, Devtool/t-shart-mania/src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is Header</h1>
-        </div>
+        <nav>
+            <Link to='/'>Home</Link>
+            <Link to='/orders'>Orders</Link>
+        </nav>
     );
 };
 
