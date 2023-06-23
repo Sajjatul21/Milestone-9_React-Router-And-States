f22b84e49a892f448aeef243b2247b0caa1e8d59
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-23 21:26:49 +0100

    added src>layouts>Main.js and set router definaiton

--- 54 Router deploy, Simple Contex ApI, Devtool/t-shart-mania/src/App.js
+++ 54 Router deploy, Simple Contex ApI, Devtool/t-shart-mania/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Main from './layouts/Main';
 
 function App() {
   const router = createBrowserRouter([
-
+    // router definition
+    {
+      path: '/',
+      element: <Main></Main>
+    }
   ]);
   return (
     <div className="App">
---
+++ 54 Router deploy, Simple Contex ApI, Devtool/t-shart-mania/src/__steps.js
@@ -0,0 +1,8 @@
+/* 
+    1. npm i react-router-dom
+    2. create a router using createBrowserRouter 
+    3. add RouterProvider and set router prop
+    4. Create a lot of component
+    5. create Main layout with some common part and one changing  based on the route
+    6.
+*/
