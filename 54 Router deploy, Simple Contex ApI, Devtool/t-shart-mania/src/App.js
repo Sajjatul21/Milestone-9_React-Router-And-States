a8cc0d6ac2094ffc3eb10fc463292c8b3005945e
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-23 21:34:55 +0100

    set router definition

--- 54 Router deploy, Simple Contex ApI, Devtool/t-shart-mania/src/App.js
+++ 54 Router deploy, Simple Contex ApI, Devtool/t-shart-mania/src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Main from './layouts/Main';
+import Home from './components/Home/Home';
+import Orders from './components/Orders/Orders';
 
 function App() {
   const router = createBrowserRouter([
     // router definition
     {
       path: '/',
-      element: <Main></Main>
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/orders',
+          element: <Orders></Orders>
+        }
+      ]
     }
   ]);
   return (
