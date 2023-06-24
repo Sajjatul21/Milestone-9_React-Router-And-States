2b920084abe82e88fc8ab023cdda698f6177c69a
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-24 16:12:17 +0100

    added class and cart component

--- 54 Router deploy, Simple Contex ApI, Devtool/t-shart-mania/src/components/Home/Home.js
+++ 54 Router deploy, Simple Contex ApI, Devtool/t-shart-mania/src/components/Home/Home.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import TShart from '../TShart/TShart';
+import Cart from '../Cart/Cart';
 
 const Home = () => {
     const tsharts = useLoaderData();
     return (
-        <div>
-            {
-                tsharts.map(tshart => <TShart
-                    key={tshart._id}
-                    tshart={tshart}
-                ></TShart>)
-            }
+        <div className='home-container'>
+            <div className="t-shart-container">
+                {
+                    tsharts.map(tshart => <TShart
+                        key={tshart._id}
+                        tshart={tshart}
+                    ></TShart>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart></Cart>
+            </div>
         </div>
     );
 };
