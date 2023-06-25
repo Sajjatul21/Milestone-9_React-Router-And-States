bf9b7bac3f5dd7cb5502127d5ed9b6f369fdb4dc
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-25 01:09:43 +0100

    conditional rendering using element and if else

--- 54 Router deploy, Simple Contex ApI, Devtool/t-shart-mania/src/components/Cart/Cart.js
+++ 54 Router deploy, Simple Contex ApI, Devtool/t-shart-mania/src/components/Cart/Cart.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const Cart = ({ cart, handleRemoveIems }) => {
-    // console.log(cart);
+
+    let message;
+    if (cart.length === 0) {
+        message = <p>Please buy at least one item !!!</p>;
+    }
+    else if (cart.length === 1) {
+        message = <div>
+            <h2>Amar jonno ekta now</h2>
+            <p>Tomar nijer jonno ekta nao</p>
+            <p><small>Tonur jonno ekta now, Please</small></p>
+        </div>;
+    }
+    else {
+        message = <p>Thanks for Buying!</p>;
+    }
     return (
         <div>
             <h2>Orders Summary</h2>
@@ -14,8 +28,15 @@ const Cart = ({ cart, handleRemoveIems }) => {
                     <button onClick={() => handleRemoveIems(tshirt)}>X</button>
                 </p>)
             }
+            {
+                message
+            }
         </div>
     );
 };
 
 export default Cart;
+/* 
+Conditional Rendering 
+    1. use element and if-else
+*/
