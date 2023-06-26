fbb7d011e64551ca68adbfab6a15d21bbd6d25e5
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-26 05:17:16 +0100

    create some component

--- 54 Router deploy, Simple Contex ApI, Devtool/t-shart-mania/src/components/Grandpa/Grandpa.js
+++ 54 Router deploy, Simple Contex ApI, Devtool/t-shart-mania/src/components/Grandpa/Grandpa.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Father from '../Father/Father';
+import Uncle from '../Uncle/Uncle';
+import Aunty from '../Aunty/Aunty';
+import './Grandpa.css';
 
 const Grandpa = () => {
     return (
-        <div>
+        <div className='grandpa'>
             <h4>Grandpa</h4>
+            <section className='flex'>
+                <Father></Father>
+                <Uncle></Uncle>
+                <Aunty></Aunty>
+            </section>
         </div>
     );
 };
