cb528657c532fb9bcb61200f5461e09b591b6b44
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-26 05:43:22 +0100

    54-6 Prop Drilling to pass data to a many level components

--- 54 Router deploy, Simple Contex ApI, Devtool/t-shart-mania/src/components/Brother/Brother.js
+++ 54 Router deploy, Simple Contex ApI, Devtool/t-shart-mania/src/components/Brother/Brother.js
@@ -4,6 +4,7 @@ const Brother = () => {
     return (
         <div>
             <h4>Brother</h4>
+            {/* 54-6 Prop Drilling to pass data to a many level components */}
         </div>
     );
 };
