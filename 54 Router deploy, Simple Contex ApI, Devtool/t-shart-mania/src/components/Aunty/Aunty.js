b36522f2548e996b0aa747dd1f3de55ed3a42976
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-26 05:27:24 +0100

    grandpa এর under এর father, uncle, aunty আছে,father,  aunty
    {myself, sister, brother} , cousign থাকে

--- 54 Router deploy, Simple Contex ApI, Devtool/t-shart-mania/src/components/Aunty/Aunty.js
+++ 54 Router deploy, Simple Contex ApI, Devtool/t-shart-mania/src/components/Aunty/Aunty.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Cousin from '../Cousin/Cousin';
 
 const Aunty = () => {
     return (
         <div>
             <h3>Aunty</h3>
+            <section className="flex">
+                <Cousin></Cousin>
+                <Cousin></Cousin>
+            </section>
         </div>
     );
 };
