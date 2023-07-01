29fd608365332759de40f2d09e7452586fd33e37
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-07-01 04:53:45 +0100

    error handling

--- Coceptual Session/smart-home-starter/src/utils/routes.js
+++ Coceptual Session/smart-home-starter/src/utils/routes.js
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "../components/Root";
 import Home from "../components/Home";
 import About from "../components/About";
+import ErrorPage from "../components/ErrorPage";
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Root />,
+        errorElement: <ErrorPage />,
         children: [
             { path: '/', element: <Home /> },
             { path: '/home', element: <Home /> },
