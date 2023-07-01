566ac81d3ff201719e95dafe7d94fda1753ab62a
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-07-01 04:46:13 +0100

    set roots

--- Coceptual Session/smart-home-starter/src/utils/routes.js
+++ Coceptual Session/smart-home-starter/src/utils/routes.js
@@ -1,10 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "../components/Home";
+import Root from "../components/Root";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Home />
+        element: <Root />
     }
 ]);
 
