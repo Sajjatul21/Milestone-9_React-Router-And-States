5658384806d1181cc6911be6fc529ce9f32e6fb6
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-07-01 04:51:08 +0100

    set Home & About as Children

--- Coceptual Session/smart-home-starter/src/utils/routes.js
+++ Coceptual Session/smart-home-starter/src/utils/routes.js
@@ -1,10 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../components/Root";
+import Home from "../components/Home";
+import About from "../components/About";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Root />
+        element: <Root />,
+        children: [
+            { path: '/', element: <Home /> },
+            { path: '/home', element: <Home /> },
+            { path: '/about', element: <About /> },
+        ]
+
     }
 ]);
 
