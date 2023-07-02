ae0b2fe9f75d318267fe4e086dc5c6e863d9a07f
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-07-02 05:32:02 +0100

    create path and loade data using context on shop component

--- Coceptual Session/smart-home-starter/src/components/Shop.jsx
+++ Coceptual Session/smart-home-starter/src/components/Shop.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { ProductContext } from './Root';
 
 const Shop = () => {
+  const products = useContext(ProductContext);
   return (
     <div className='px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20'>
       <div className='grid gap-8 row-gap-5 mb-8 lg:grid-cols-3 lg:row-gap-8'>
-        <h1>Shop Component</h1>
+        <h1>Shop Component {products.length}</h1>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Shop
+export default Shop;
--- Coceptual Session/smart-home-starter/src/utils/routes.js
+++ Coceptual Session/smart-home-starter/src/utils/routes.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../components/Root";
 import Home from "../components/Home";
+import Shop from "../components/Shop";
 import About from "../components/About";
 import ErrorPage from "../components/ErrorPage";
 
@@ -9,10 +10,11 @@ const router = createBrowserRouter([
         path: '/',
         element: <Root />,
         errorElement: <ErrorPage />,
-        loader:()=>fetch('products.json'),
+        loader: () => fetch('products.json'),
         children: [
             { path: '/', element: <Home /> },
             { path: '/home', element: <Home /> },
+            { path: '/shop', element: <Shop /> },
             { path: '/about', element: <About /> },
         ]
 
