309cc824af04dbf6345489c111f94792316f626e
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-07-03 04:08:42 +0100

    we click the product if the products is not in cart  we   set product quentity is 1 and set the cart and product.
    if product is already avelable we increase the product quentuty  and set the product .then we set local storage then added tostyfy

--- Coceptual Session/smart-home-starter/src/components/Shop.jsx
+++ Coceptual Session/smart-home-starter/src/components/Shop.jsx
@@ -1,14 +1,34 @@
 import React, { useContext, useState } from 'react';
 import { ProductContext } from './Root';
 import Product from './Product';
+import { addToDb } from '../utils/fakeDB';
+import { toast } from 'react-toastify';
 
 const Shop = () => {
   const products = useContext(ProductContext);
   const [cart, setCart] = useState([]);
-  console.log(cart);
+
   const handleAddToCart = (product) => {
-    setCart(previous => [...previous, product]);
+    let newCart = [];
+    const exist = cart.find(existingProducts => existingProducts.id === product.id);
+    // console.log(exist);
+
+    if (!exist) {
+      product.quantity = 1;
+      newCart = [...cart, product];
+    }
+    else {
+      const rest = cart.filter(existingProducts => existingProducts.id !== product.id);
+      exist.quantity = exist.quantity + 1;
+      exist.quantity = exist.quantity + 1;
+      newCart = [...rest, exist];
+    }
+    setCart(newCart);
+    addToDb(product.id);
+    toast.success('Product Added!', { autoClose: 500 });
   };
+
+
   return (
     <div className='px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20'>
       <div className='grid gap-8 row-gap-5 mb-8 lg:grid-cols-3 lg:row-gap-8'>
