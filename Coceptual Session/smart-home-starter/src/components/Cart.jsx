710bc8acdcda3618be17fd5f67c0edc2bc725f43
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-07-10 08:16:29 -0700

    calculate total

--- Coceptual Session/smart-home-starter/src/components/Cart.jsx
+++ Coceptual Session/smart-home-starter/src/components/Cart.jsx
@@ -15,6 +15,11 @@ const Cart = () => {
     toast.warning('Product Removed!', { autoClose: 5000 });
   };
 
+  let total = 0;
+  for (const product of cart) {
+    total = total + product.price * product.quantity;
+  }
+
   return (
     <div className='flex min-h-screen items-start justify-center bg-gray-100 text-gray-900'>
       <div className='flex flex-col max-w-3xl p-6 space-y-4 sm:p-10 '>
@@ -28,7 +33,7 @@ const Cart = () => {
         </ul>
         <div className='space-y-1 text-right'>
           <p>
-            Total amount: <span className='font-semibold'>00$</span>
+            Total amount: <span className='font-semibold'>{total}$</span>
           </p>
           <p className='text-sm text-gray-400'>
             Not including taxes and shipping costs
