bdebbdcb9429a452284aea63e509129ca503384f
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-07-26 23:37:25 -0700

    make another api

--- 55.5 React Router Bonous Content/iphon-hunter-server/index.js
+++ 55.5 React Router Bonous Content/iphon-hunter-server/index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('Look mama nodemon!!! Iphone server running');
 });
 
+app.get('/phones', (req, res) => {
+    res.send('Phone data is coming soon!');
+});
+
 app.listen(port, () => {
     console.log(`Iphone server is running on port:${port}`);
 });
